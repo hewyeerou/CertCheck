2a49c2b502594a61f49d44d5cb6d26e6f1978a13
hewyeerou <e0417583@u.nus.edu>
2022-03-04 22:06:00 +0800

    changed struct to React Hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,78 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+function App() {
+    // initialize the state variables of the application
+    const [storageValue, setStorageValue] = useState(0);
+    const [web3, setWeb3] = useState();
+    const [accounts, setAccounts] = useState();
+    const [contract, setContract] = useState();
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+    // equivalent to the componentDidMount function of older React frameworks
+    useEffect(() => {
+      const init = async () => {
+          try {
+              // Get network provider (typically MetaMask) and web3 instance
+              const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+              // Use web3 to get the user's accounts from the provider (MetaMask)
+              const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+              // Get the contract instance
+              const networkId = await web3.eth.net.getId();
+              const deployedNetwork = SimpleStorageContract.networks[networkId];
+              const instance = new web3.eth.Contract(
+                  SimpleStorageContract.abi,
+                  deployedNetwork && deployedNetwork.address,
+              );
+              // Set web3, accounts, contract to the state
+              setWeb3(web3);
+              setContract(instance);
+              setAccounts(accounts);
+          } catch (error) {
+              // Catch any errors for any of the above operations
+              alert(
+                  `Failed to load web3, accounts, or contract. Did you migrate the contract or install MetaMask? Check console for details.`,
+              );
+              console.error(error);
+          }
+      };
+      init();
+    }, []);
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+    // is called whenever there was any change in the state variables web3, accounts, contract
+    useEffect(() => {
+      const runExample = async () => {
+          // example of interaction with the smart contract
+          try{
+              // Stores a given value, 5 by default
+              await contract.methods.set(5).send({ from: accounts[0] });
+
+              // Get the value from the contract to prove it worked
+              const response = await contract.methods.get().call();
+
+              // Update state with the result
+              setStorageValue (response);
+          }
+          catch{
+              alert('No contract deployed or account error; please check that MetaMask is on the correct network, reset the account and reload page');
+          }
+      }
+      if(typeof(web3) != 'undefined'
+          && typeof(accounts) != 'undefined'
+          && typeof(contract) != 'undefined'){
+          runExample();
+      }
+    }, [web3, accounts, contract]);
+
+    if (typeof(web3) === 'undefined') {
+        return <div className="App">Loading Web3, accounts, and contract... Reload page</div>;
     }
-  };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+    // equivalent to the render function of older React frameworks
     return (
       <div className="App">
         <h1>Good to Go!</h1>
@@ -64,10 +85,10 @@ class App extends Component {
         <p>
           Try changing the value stored on <strong>line 42</strong> of App.js.
         </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>The stored value is: {storageValue}</div>
       </div>
     );
-  }
+
 }
 
 export default App;
